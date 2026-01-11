7346fe37d556491b7932b6349f09a4089699e051
Erik Dubois <erik.dubois@gmail.com>
2026-01-11 06:59:45 +0100

    update

--- up.sh
+++ up.sh
@@ -26,7 +26,7 @@ set -eo pipefail
 
 # variables and functions
 workdir=$(pwd)
-dir="calamares-3.3.14.r87.g3f6cd83-1"
+dir="calamares-next-3.3.14.r87.g3f6cd83-1"
 source="/home/erik/KIRO/kiro-pkgbuild/"
 destiny="/home/erik/KIRO/kiro-calamares-config-next/etc/calamares/pkgbuild/"
 
